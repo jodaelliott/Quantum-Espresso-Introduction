ab953eceb46fefbc1ac3666d80f82993a71bacb0
jodaelliott <jodaelliott@gmail.com>
2025-11-04 21:31:02 +0000

    Updates to lecture 02

--- Examples/02_Introduction_to_SCARF/Submitting_Batch_Jobs/job.sh
+++ Examples/02_Introduction_to_SCARF/Submitting_Batch_Jobs/job.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH -p scarf
-#SBATCH -C amd
-#SBATCH --nodes 1 --ntasks-per-node=32
+#SBATCH --nodes 1 --ntasks=16 --ntasks-per-node=16 --cpus-per-task=2
 #SBATCH -t 10:00
 #SBATCH -o job_%J.log
 #SBATCH -e job_%J.err
 
 export OMP_NUM_THREADS=1
-module load contrib/dls-spectroscopy/quantum-espresso/7.3.1-GCC-12.2.0
+module load contrib/dls-spectroscopy/quantum-espresso/7.4.1-GCC-14.2.0
 
-mpirun -np ${SLURM_NTASKS} pw.x -inp c60_scf.pwi > c60_scf.pwo
+srun pw.x -inp c60_scf.pwi > c60_scf.pwo
